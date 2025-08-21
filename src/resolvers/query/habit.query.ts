4a9ff58be48a59193a678f6173d19b1c12a68b8c
Андрей Романов <human1488pwnz@yandex.ru>
2025-08-21 22:36:11 +0300

    feat: add habitsByUser query and reorder habits query

--- src/resolvers/query/habit.query.ts
+++ src/resolvers/query/habit.query.ts
@@ -3,6 +3,7 @@ import type { QueryResolvers } from '../../__generated__/types'
 
 export const HabitQuery: QueryResolvers = {
   health: () => 'ok',
-  habits: async () => Habit.find().lean(),
   habit: async (_p, { id }) => Habit.findById(id).lean() ?? null,
+  habits: async () => Habit.find().lean(),
+  habitsByUser: async (_p, { userId }) => Habit.find({ userId }).lean(),
 }
