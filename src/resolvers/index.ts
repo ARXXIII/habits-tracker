5c0b0e2efe33f3f3d9246c3cfcf89725f524411b
Андрей Романов <human1488pwnz@yandex.ru>
2025-08-21 22:34:48 +0300

    feat: user resolver

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -9,11 +9,15 @@ export const resolvers: Resolvers = {
   Query: query,
   Mutation: mutation,
 
+  User: {
+    id: (h) => String((h as any)._id ?? (h as any).id),
+    habits: (src, _args, ctx) => ctx.loaders.habitsByUser.load(String(src._id)),
+  },
   Habit: {
     id: (h) => String((h as any)._id ?? (h as any).id),
+    userId: (h) => String((h as any).userId),
     logs: (src, _args, ctx) => ctx.loaders.logsByHabit.load(String(src._id)),
   },
-
   HabitLog: {
     id: (l) => String((l as any)._id ?? (l as any).id),
     habitId: (l) => String((l as any).habitId),
