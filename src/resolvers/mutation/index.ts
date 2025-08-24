1bfce27f0e711199a7634579e9f3569f970b608c
Андрей Романов <human1488pwnz@yandex.ru>
2025-08-24 17:08:32 +0300

    feat: implement authentication mutations for user registration, login, refresh token, and logout

--- src/resolvers/mutation/index.ts
+++ src/resolvers/mutation/index.ts
@@ -1,7 +1,9 @@
+import { AuthMutation } from './auth'
 import { UserMutation } from './user.mutation'
 import { HabitMutation } from './habit.mutation'
 
 export const mutation = {
+  ...AuthMutation,
   ...UserMutation,
   ...HabitMutation,
 }
