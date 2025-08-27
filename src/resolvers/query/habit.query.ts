cfffe2b52502e5436533e8c9c9d90171021868e3
Андрей Романов <human1488pwnz@yandex.ru>
2025-08-27 17:37:29 +0300

    feat: update habits resolver to require authentication and handle userId appropriately

--- src/resolvers/query/habit.query.ts
+++ src/resolvers/query/habit.query.ts
@@ -1,5 +1,7 @@
 import { Habit } from '../../models/habit'
 import { HabitLog } from '../../models/habit-log'
+
+import { requireAuth } from '../../context'
 import type { QueryResolvers } from '../../__generated__/types'
 
 export const HabitQuery: QueryResolvers = {
@@ -15,6 +17,14 @@ export const HabitQuery: QueryResolvers = {
       ...habit.toObject(),
     }
   },
-  habits: async (_p, { userId }) => Habit.find({ userId }).lean(),
+  habits: async (_p, { userId }, ctx) => {
+    const authUser = requireAuth(ctx)
+
+    if (userId) {
+      return Habit.find({ userId }).lean()
+    }
+
+    return await Habit.find({ userId: authUser._id }).lean()
+  },
   habitLogs: async (_p, { habitId }) => HabitLog.find({ habitId }).lean(),
 }
